a4970fceb1ab5b3b4fc682c5d569ff3dc4f3721d
DyanaFurriel <dyanafurriel.dsf@gmail.com>
2022-07-12 11:25:50 -0300

    Ingles

---
+++ script.js
@@ -0,0 +1,21 @@
+//Crie 3 variáveis, cada uma com um array:
+- Comedy
+let comedia = [{
+    title: 'As Branquelas',
+    diretor: 'Keenen Ivory Wayans',
+    year: 2004
+}]
+
+-Action
+let Action = [{
+    title: 'Duro de Matar',
+    diretor: 'John McTiean',
+    year: 1989
+}]
+
+-Fantasy 
+let fantasy = [{
+    title: 'Alice no Pais das Maravilhas',
+    diretor: 'Tim Burton',
+    year: 2010
+}]
